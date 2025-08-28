8ed1044fba75da25b9f4083e0024d9893ac6dae0
CodeLikeKitten <CodeLikeKitten@github.com>
2025-08-28 04:36:49 +0400

    Начало положено

---
+++ HarpMate/build/Desktop-Debug/CMakeFiles/HarpMate.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HarpMate_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/HarpMate_autogen.dir/ParseCache.txt"
+  "HarpMate_autogen"
+  "CMakeFiles/HarpMate.dir/HarpMate_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/HarpMate.dir/HarpMate_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/HarpMate.dir/main.cpp.o"
+  "CMakeFiles/HarpMate.dir/main.cpp.o.d"
+  "CMakeFiles/HarpMate.dir/mainwindow.cpp.o"
+  "CMakeFiles/HarpMate.dir/mainwindow.cpp.o.d"
+  "HarpMate"
+  "HarpMate.pdb"
+  "HarpMate_autogen/mocs_compilation.cpp"
+  "HarpMate_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HarpMate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
